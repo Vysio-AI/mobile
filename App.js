633dd19e2940db3d6a7d17841683255b7d7d9c2d
Peter Marshall <peterdmarshall99@gmail.com>
2021-10-15 11:33:05 -0400

    First commit

---
+++ App.js
@@ -0,0 +1,71 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  Button,
+  View,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import tailwind from 'tailwind-rn';
+import DeviceInfo from 'react-native-device-info';
+import Auth0 from 'react-native-auth0';
+import Config from 'react-native-config';
+
+// Import context
+import AppContext from './lib/AppContext';
+
+// Import Screens
+import MainScreen from './lib/screens/MainScreen';
+import LoginScreen from './lib/screens/LoginScreen';
+import ReferralScreen from './lib/screens/ReferralScreen';
+
+const auth0 = new Auth0({
+  domain: 'petermarshall.us.auth0.com',
+  clientId: 'SnjygAyEh3ufh8uXz0gmRM2F6O79Lf2J',
+});
+
+const Stack = createNativeStackNavigator();
+
+// Create React context for storing auth/user data
+const initialContext = {
+  auth0: auth0,
+  userInfo: null,
+  accessToken: null,
+};
+
+const App = () => {
+  const [userInfo, setUserInfo] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
+
+  return (
+    <AppContext.Provider value={{
+      auth0: auth0,
+      userInfo: userInfo,
+      setUserInfo: setUserInfo,
+      accessToken: accessToken,
+      setAccessToken: setAccessToken,
+    }}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Referral" component={ReferralScreen} />
+          <Stack.Screen name="Main" component={MainScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AppContext.Provider>
+  );
+};
+
+export default App;
